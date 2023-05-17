8057c59edf62bb374850e467cc069622da425138
Himanshu Kesharwani <himanshu.kesharwani.02@gmail.com>
2023-05-17 12:35:59 +0530

    [UPDATE]: Implemented git sub-moduling feature.

--- GitSubModule-Demo/GitSubModule-Demo/ViewController.swift
+++ GitSubModule-Demo/GitSubModule-Demo/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //test()
+        
         // Do any additional setup after loading the view.
     }
-
 
 }
 
